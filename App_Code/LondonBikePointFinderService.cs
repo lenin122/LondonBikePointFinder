120a418026ed599e47875f8a96e854b514ee13f6
lenin122 <lenin122@gmail.com>
2016-09-22 13:40:04 +0300

    First Commit

---
+++ App_Code/LondonBikePointFinderService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+/// <summary>
+/// Summary description for LondonBikePointFinderService
+/// </summary>
+[WebService(Namespace = "http://lenin122testapp.somee.com/")]
+[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+// To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+// [System.Web.Script.Services.ScriptService]
+public class LondonBikePointFinderService : System.Web.Services.WebService
+{
+
+    public LondonBikePointFinderService()
+    {
+
+        //Uncomment the following line if using designed components 
+        //InitializeComponent(); 
+    }
+
+    [WebMethod]
+    public string HelloWorld()
+    {
+        return "Hello World";
+    }
+
+}
