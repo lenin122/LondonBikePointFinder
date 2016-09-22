52265e02af68861027052df52eb3c61da20e4ff7
lenin122 <lenin122@gmail.com>
2016-09-22 21:34:41 +0300

    WebService debug started

--- App_Code/LondonBikePointFinderService.cs
+++ App_Code/LondonBikePointFinderService.cs
@@ -1,8 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+using System.Globalization;
+using System.Net;
+using System.Net.Http;
 using System.Web.Services;
+using LondonBikePointFinderServiceApp.Models;
+using System.Text;
+using System.Web.Configuration;
+using System.Web.Script.Services;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 /// <summary>
 /// Summary description for LondonBikePointFinderService
@@ -10,21 +19,151 @@ using System.Web.Services;
 [WebService(Namespace = "http://lenin122testapp.somee.com/")]
 [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
 // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
-// [System.Web.Script.Services.ScriptService]
+[ScriptService]
 public class LondonBikePointFinderService : System.Web.Services.WebService
 {
+    private const string ConnectionStringName = "LONDONBASE";
+
+    [ScriptMethod(ResponseFormat = ResponseFormat.Json)]
+    [WebMethod(Description = "Refreshing top viewed BikePoints in radius search")]
+    public string RefreshTopPoints(int topValue)
+    {
+        using (var sqlConnection = new SqlConnection(WebConfigurationManager.ConnectionStrings[ConnectionStringName].ConnectionString))
+        {
+            if (sqlConnection.State == System.Data.ConnectionState.Closed)
+                sqlConnection.Open();
+            var sqlCommand = new SqlCommand(@"
+                                            SELECT TOP (@top_value)
+                                                [commonName],[counter] 
+                                            FROM
+                                                [LONDONBASE].[dbo].[BIKEPOINTS]
+                                            ORDER BY counter desc", sqlConnection);
+            sqlCommand.Parameters.Add("@top_value", SqlDbType.SmallInt).Value = topValue.ToString();
+            var resultSB = new StringBuilder();
+
+            resultSB.Append(@"<table id='top_table'>");
+            using (var sqlDataReader = sqlCommand.ExecuteReader())
+            {
+                while (sqlDataReader.Read())
+                {
+                    resultSB.Append(String.Format(@"<tr><td>{0}</td></tr>",
+                        sqlDataReader.GetString(0), sqlDataReader.GetInt32(1).ToString()));
+                }
+            }
+            
+            resultSB.Append(@"</table>");
+            return resultSB.ToString();
+        }
+
+    }
+
+    [ScriptMethod(ResponseFormat = ResponseFormat.Json, UseHttpGet = true)]
+    [WebMethod(Description = "This method gets all nike points in london") ]
+    public async Task<string> GetAllBikeStops()
+    {
+
+        try
+        {
+            List<BikePoints> bikePoints_List;
+            HttpClient client = new HttpClient();
+
+            HttpResponseMessage httpmessage = await client.GetAsync("https://api.tfl.gov.uk/BikePoint?app_id=57e5756c&app_key=fa0bd9e6338c902e6ef998ff24fd5607").ConfigureAwait(continueOnCapturedContext: false);
+            httpmessage.EnsureSuccessStatusCode();
+            string response = await httpmessage.Content.ReadAsStringAsync();
+            //geted string
+            bikePoints_List = await System.Threading.Tasks.Task.Factory.StartNew(() =>
+                    JsonConvert.DeserializeObject<List<BikePoints>>(response));
+            //make small copy in memory for jquery
+            return await System.Threading.Tasks.Task.Factory.StartNew(() =>
+                    JsonConvert.SerializeObject(bikePoints_List, Formatting.None)); //, Formatting.Indented));
+        }
+        catch (Exception ex)
+        {
+           return JsonConvert.SerializeObject(ex, Formatting.None);
+        }
+
+        
+    }
+
+    [WebMethod(Description = "Returns json string with bike points in request radius around request lat/lng")]
+    public async Task<string> GetPointsWithRadius(double lat, double lon, double radius)
+    {
+        
+        StringBuilder answer = new StringBuilder();
+        //getting json from webapi
+        
+        string url = String.Format("https://api.tfl.gov.uk/BikePoint?lat={0:0.####}&lon={1:1.####}&radius={2}&app_id=57e5756c&app_key=fa0bd9e6338c902e6ef998ff24fd5607",
+            lat.ToString(CultureInfo.GetCultureInfo("en-US").NumberFormat), lon.ToString(CultureInfo.GetCultureInfo("en-US").NumberFormat),
+            radius);
+
+        var httpClient = new HttpClient();
+        var httpmessage = await httpClient.GetAsync(url);
+        httpmessage.EnsureSuccessStatusCode();
+        var response = await httpmessage.Content.ReadAsStringAsync();
+        //get answer, and then  link list to answer.list
+        var pm = JsonConvert.DeserializeObject(answer.ToString(), typeof(PropertyAnswerModel));
+        //count++ in sql for all points in bikePoints_list
+        IncrementPoints(((PropertyAnswerModel)pm).places);
+        //return jsonstring to show this points
+        answer.Clear();
+        answer.Append(JsonConvert.SerializeObject(((PropertyAnswerModel)pm).places, Formatting.None));
+        return answer.ToString();
+    }
+
+    private void IncrementPoints(List<BikePoints> items)
+    {
+        SqlConnection connection = new SqlConnection(WebConfigurationManager.ConnectionStrings[ConnectionStringName].ConnectionString);
+        SqlTransaction transaction = null;
+        bool everythingisgreat = true;
+        try
+        {
+
+            if (connection.State == System.Data.ConnectionState.Closed)
+                connection.Open();
+            transaction = connection.BeginTransaction();
+            foreach (BikePoints bp in items)
+            {
+                SqlCommand command = new SqlCommand(@"IF EXISTS(
+                                                            SELECT commonName FROM [LONDONBASE].[dbo].[BIKEPOINTS] 
+                                                            WHERE commonName = @commonName)
+                                                            --value exists perform update
+                                                            BEGIN
+                                                                UPDATE [LONDONBASE].[dbo].[BIKEPOINTS]
+					                                            SET [counter] = [counter] + 1
+					                                            WHERE commonName = @commonName
+                                                            END
+                                                            ELSE
+                                                            --value doesnt exist perform insert
+                                                            BEGIN
+                                                                INSERT INTO [LONDONBASE].[dbo].[BIKEPOINTS]
+                                                                    ([commonName],[counter])
+                                                                VALUES
+                                                                    (@commonName, 1)
+                                                            END", connection);
+                command.Parameters.Add("@commonName", SqlDbType.VarChar).Value = bp.commonName;
+                command.Transaction = transaction;
+                everythingisgreat &= command.ExecuteNonQuery() == 1;
+            }
+            if (everythingisgreat)
+                transaction.Commit();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+            transaction.Rollback();
+        }
+        finally
+        {
+            connection.Close();
+        }
+    }
+
 
     public LondonBikePointFinderService()
     {
 
         //Uncomment the following line if using designed components 
         //InitializeComponent(); 
-    }
-
-    [WebMethod]
-    public string HelloWorld()
-    {
-        return "Hello World";
     }
 
 }
---
+++ scripts/bikepoints.js
@@ -0,0 +1,208 @@
+﻿var bikepointsSC = (function () {
+    var map;
+    var dblClkMarker;
+    var markers = [];
+    var painting = false;
+    var initMapfunc = function(){
+        var myOptions = {
+            zoom: 10, disableDoubleClickZoom: true,
+            center: new google.maps.LatLng(51.5073509, -0.12775829999998223),
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        map = new google.maps.Map($('#gmap_canvas')[0], myOptions);
+        $("#gmap_canvas").height(($(window).height() - $("#up_part").height()) * 0.9);
+        $("#foundedBikePointsTable").tablesorter();
+        //add listener for dblclick
+        addDblClkListerFunc();
+        //init change top points event
+        $("#TopPointsInput").change(refreshTopPoints);
+        //init clear all markers button
+        $("#ClearMarkersButton").click(clearMarkers);
+        //init show all markers button
+        $("#GetAllMarkers").click(ajaxRequestAllPoints);//getJSonString);
+        refreshTopPoints();
+        //set wait gif
+        $body = $("body");
+
+        $(document).on({
+            ajaxStart: function () { $body.addClass("loading"); },
+            ajaxStop: function () { $body.removeClass("loading"); }
+        });
+        
+    }
+
+    var addDblClkListerFunc = function () {
+        dblClkMarker = new google.maps.Marker({ map: map });
+        map.addListener("dblclick", function (event) {
+            //dblClkMarker.setPosition(event.latLng);
+            showPointsInRadius(event, dblClkMarker);
+        });
+    }
+
+    var setMapOnAll = function (work_map) {
+        for (var i = 0; i < markers.length; i++) {
+            markers[i].setMap(work_map);
+        }
+    }
+
+    var clearMarkers = function () {
+        setMapOnAll(null);
+    }
+
+    var deleteMarkers = function () {
+        clearMarkers();
+        markers = [];
+    }
+
+    var ajaxRequestAllPoints = function () {
+        clearMarkers();
+        $.ajax({
+            type: 'GET',
+            url: '/LondonBikePointFinderService.asmx/GetAllBikeStops',
+            contentType: 'application/json; charset=utf-8',
+            dataType: 'json',
+            success: function onSuccessJSONGet(response, status) {
+                PaintMarkers($.parseJSON(response.d.Result));
+            },
+            error: function (data) {
+                console.write(data);
+            }
+        });
+    }
+
+    var initTableDivFunc = function () {
+        $('#top_table_div').html("Working");
+    }
+
+    var PaintMarkers = function (data) {
+        var Bounds = new google.maps.LatLngBounds();
+        var tableData = "<table id=\"foundedBikePointsTable\" class=\"tablesorter\"><thead><tr><th>BikePoint Name</th>" 
+            +"<th>Free bikes</th><th>Free docks</th></tr></thead><tbody>";
+        $.each(data, function () {
+            //set marker color
+            //greener - have free bikes
+            //red - no bikes available
+            if (this.additionalProperties[1].value == "true" &&
+                this.additionalProperties[2].value == "false") {
+                var part = Math.floor((this.additionalProperties[6].value / this.additionalProperties[8].value) * 255);
+                var pinColor
+                if (part >= 0 && part < 16) {
+                    pinColor = "0" + part.toString(16).toUpperCase() +
+                        (255 - part).toString(16).toUpperCase() + "00";
+                }
+                else if (part <= 255 && part > 239) {
+                    pinColor = "" + (part).toString(16).toUpperCase() + "0"
+                        + (255 - part).toString(16).toUpperCase() + "00";
+                }
+                else if (part >= 16 && part <= 239) {
+                    pinColor = "" + (part).toString(16).toUpperCase()
+                        + (255 - part).toString(16).toUpperCase() + "00";
+                }
+                var pinImage = new google.maps.MarkerImage("http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|"
+                    + pinColor, new google.maps.Size(21, 34), new google.maps.Point(0, 0), new google.maps.Point(10, 34));
+                var marker = new google.maps.Marker({ map: map, icon: pinImage });
+                marker.setPosition({ lat: this.lat, lng: this.lon });
+                Bounds.extend({ lat: this.lat, lng: this.lon });
+                var infoString = "At " + this.commonName + " " + this.additionalProperties[7].value
+                    + " bikes availible";
+                var infowindow = new google.maps.InfoWindow({
+                    content: infoString
+                });
+                marker.addListener('click', function () {
+                    infowindow.open(map, marker);
+                });
+                markers.push(marker);
+                tableData += "<tr>";
+                tableData += "<td>";
+                tableData += this.commonName;
+                tableData += "</td>";
+                tableData += "<td>";
+                tableData += this.additionalProperties[6].value;
+                tableData += "</td>";
+                tableData += "<td>";
+                tableData += this.additionalProperties[7].value;
+                tableData += "</td>";
+                tableData += "</tr>";
+            }
+            else {
+                console.log(this.commonName + " locked or not onstalled");
+            }
+        });
+        if (Bounds.isEmpty() == false) {
+            map.setCenter(Bounds.getCenter(), map.fitBounds(Bounds));
+            tableData += "</tbody></table>"
+            $("#foundedBikePointsTableDiv").html(tableData);
+            $("#foundedBikePointsTable").tablesorter();
+        }
+    }
+
+    var refreshTopPoints = function () {
+        var topPointsToShow = $("#TopPointsInput")[0].value;
+        if (topPointsToShow > 0) {
+            $.ajax({
+                type: 'POST',
+                url: '/LondonBikePointFinderService.asmx/RefreshTopPoints',
+                data: JSON.stringify({ topValue: topPointsToShow }),
+                contentType: 'application/json; charset=utf-8',
+                dataType: 'json',
+                success: function onSuccessRefreshGet(response, status) {
+                    $('#top_table_div').html(response.d);
+                }
+            });
+        }
+        else
+        {
+            console.log("Check input top points value");
+        }
+    }
+
+    var showPointsInRadius = function (event, marker) {
+        var radius_value = $("#RadiusInput")[0].value;
+        if (painting === false && radius_value > 1) {
+            //check radius            
+            painting = true;
+            console.log("Painting if: " + painting);
+            marker.setPosition(event.latLng);
+            clearMarkers();
+            $.ajax({
+                type: 'POST',
+                url: '/LondonBikePointFinderService.asmx/GetPointsWithRadius',
+                data: JSON.stringify({ lat: marker.getPosition().lat(), lon: marker.getPosition().lng(), radius: radius_value }),
+                contentType: 'application/json; charset=utf-8',
+                dataType: 'json',
+                success: function onSuccessRefreshGet(response, status) {
+                    var objs = $.parseJSON(response.d);
+                    if (objs.length > 0) {
+                        PaintMarkers(objs);
+                        refreshTopPoints();
+                    }
+                    else {
+                        alert("Nothing was found");
+                    }
+                    painting = false;
+                },
+                error: function(){
+                    painting = false;
+                }
+            });
+        }
+        else {
+            console.log("I'm not painting. Or i'm already painting, or check radius input");
+        };
+
+    }
+
+    var getJSonString = function () {
+        var url_get = "https://api.tfl.gov.uk/BikePoint?app_id=57e5756c&app_key=fa0bd9e6338c902e6ef998ff24fd5607"
+        $.getJSON(url_get, function (data) {
+            PaintMarkers(data);
+        });
+    }
+
+    return {
+        initMap: initMapfunc,
+        initTableDiv: initTableDivFunc
+    }
+}
+)();
+$(document).ready(bikepointsSC.initMap);
